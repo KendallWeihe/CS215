5a7d6be5eecde1c5661ccb6f535f76fae2935d0c
Kendall Weihe <kendallweihe@gmail.com>
2016-09-10 12:33:18 -0400

    added lab1

---
+++ lab1/lab1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	cout << "Hello World!\n";
+	cout << "Name: Kendall Weihe\n";
+	cout << "Hometown: Goshen, KY\n";
+	cout << "Something interesting: I grew up with three sisters!\n";
+
+	system("pause");
+	return 0;
+
+}
