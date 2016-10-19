9d6bb88a55496b98fa261698a21a365133de5f0f
Kendall Weihe <kendallweihe@gmail.com>
2016-10-18 21:06:36 -0400

    can't figure this out, whatever{

--- lab6/lab6.cpp
+++ lab6/lab6.cpp
@@ -1,26 +1,49 @@
+// ----------------------------------------------------
+//         Trajectory Calculator
+// ----------------------------------------------------
+// Author:   Kendall Weihe
+// Course:   CS215 Fall 2016
+// Date:     October 18th, 2016
+//
+// This program computes the trajectory of an object launched
+// at 100m/s -- the user is able to define the angle
+
+
 #include <iostream>
 #include <math.h>
 #include <iomanip>
 
 using namespace std;
 
+// function declarations
 void InitMeasurements(double list[]);
 int CalcTrajectory(double distances[], double heights[], double angle, double velocity);
 void PrintTrajectory(double distances[], double heights[], int num_intervals);
-double MaxMeasurement(double list[], int num_intervals);
+double MaxMeasurement(double list[]);
 
 const int MAX_NUM_ELEMENTS = 100;
 const int NUM_INTERVALS = 15;
 
+// -------------------------------------------------------
+//      main()
+// -------------------------------------------------------
+// Arguments:
+//   None
+// Returns:
+//   0 if successful
+// -------------------------------------------------------
 int main(){
 
+  // read user input for degrees
   double degrees;
   cout << "Enter the angle in degrees <15-75>: ";
   cin >> degrees;
 
+  // convert degrees to radians -- math functions
   double radians;
   radians = (degrees * M_PI) / 180;
 
+  // initialize distances and heights arrays
   double distances[MAX_NUM_ELEMENTS], heights[MAX_NUM_ELEMENTS];
   InitMeasurements(distances);
   InitMeasurements(heights);
@@ -28,6 +51,7 @@ int main(){
   double velocity = 100.0;
   int num_intervals;
 
+  // calculate and print trajectory data
   num_intervals = CalcTrajectory(distances, heights, radians, velocity);
   PrintTrajectory(distances, heights, num_intervals);
 
@@ -36,6 +60,14 @@ int main(){
 
 }
 
+// -------------------------------------------------------
+//     InitMeasurements()
+// -------------------------------------------------------
+// Arguments:
+//   - a list of doubles
+// Returns:
+//   - nothing -- list is passed by reference
+// -------------------------------------------------------
 void InitMeasurements(double list[]){
 
   for (int i = 0; i < MAX_NUM_ELEMENTS; i++){
@@ -44,41 +76,72 @@ void InitMeasurements(double list[]){
 
 }
 
+// -------------------------------------------------------
+//     CalcTrajectory()
+// -------------------------------------------------------
+// Arguments:
+//   - list of doubles for distances
+//   - list of doubles for heights
+//   - angle in radians
+//   - velocity in m/s
+// Returns:
+//   - number of time intervals
+// -------------------------------------------------------
 int CalcTrajectory(double distances[], double heights[], double angle, double velocity){
 
-  int t = 0;
+  double t = 0;
   for (int i = 1; i < NUM_INTERVALS; i++){
     t++;
-    distances[i] = velocity * cos(angle) + distances[i-1];
-    heights[i] = velocity * sin(angle) - (9.80665 * pow(t,2)) / 2 + heights[i-1];
-    if (heights[i] < 0){
+    distances[i] = velocity * cos(angle) + distances[i-1]; // compute distance
+    heights[i] = velocity * sin(angle) - (9.80665 * pow(t,2) * 0.5) + heights[i-1]; // compute height
+    if (heights[i] < 0){ // if the height has reached the ground, then break
       break;
       t--;
     }
   }
 
-  return t;
+  return t; // return the number of time intervals
 
 }
 
+// -------------------------------------------------------
+//     PrintTrajectory()
+// -------------------------------------------------------
+// Arguments:
+//   - list of doubles for distances
+//   - list of doubles for heights
+// Returns:
+//   - nothing
+// -------------------------------------------------------
 void PrintTrajectory(double distances[], double heights[], int num_intervals){
 
+  // print distance and heigh for each interval
   for (int i = 0; i < num_intervals; i++){
     cout << setw(4) << i << fixed << setprecision(3) << setw(10) << distances[i] << setw(10) << heights[i] << endl;
   }
 
+  // find max height
   double max_height;
-  max_height = MaxMeasurement(heights, num_intervals);
+  max_height = MaxMeasurement(heights);
 
+  // print max height
   cout << "Maximum height: " << max_height << endl;
 
 }
 
-double MaxMeasurement(double heights[], int num_intervals){
+// -------------------------------------------------------
+//     MaxMeasurement()
+// -------------------------------------------------------
+// Arguments:
+//   - list of doubles for heights
+// Returns:
+//   - maximum height
+// -------------------------------------------------------
+double MaxMeasurement(double heights[]){
 
   double max_height = 0;
-  for (int i = 0; i < num_intervals; i++){
-    if (heights[i] > max_height){
+  for (int i = 0; i < NUM_INTERVALS; i++){
+    if (heights[i] > max_height){ // king of the hill
       max_height = heights[i];
     }
   }
